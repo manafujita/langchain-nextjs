8f1a32a414472e6f0e609d836375a08c03b4cff5
manafujita <mana.fujita.tech@gmail.com>
2024-07-23 10:30:35 +0900

    updated ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="no-js">{children}</body>
     </html>
   );
 }
